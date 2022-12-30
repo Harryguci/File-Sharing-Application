4f3eab1bdaadebc12c7bdc99c21d07b374a3b5b6
Harryguci <huyquang0028@gmail.com>
2022-12-30 13:10:12 +0700

    Added searching

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const sharingController = require("../app/controllers/sharingController");
 const uploadRouter = require("./upload");
 const filesRouter = require("./files");
+const searchRouter = require("./search");
 
 router.use("/upload", uploadRouter);
 router.use("/files", filesRouter);
+router.use("/search", searchRouter);
 router.get("/about", sharingController.about);
 router.get("/all", sharingController.showJson);
 router.get("/", sharingController.show);
